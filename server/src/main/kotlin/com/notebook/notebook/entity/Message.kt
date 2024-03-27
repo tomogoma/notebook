8d82a40693578961a5b77d969d39575bedd4c530
Ogoma <ogomatom@gmail.com>
2024-03-27 08:11:55 +0300

    explicit definition of table and column sql characters

--- server/src/main/kotlin/com/notebook/notebook/entity/Message.kt
+++ server/src/main/kotlin/com/notebook/notebook/entity/Message.kt
@@ -1,36 +1,38 @@
 package com.notebook.notebook.entity
 
 import com.notebook.notebook.common.MessageType
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import lombok.Data
-import lombok.NoArgsConstructor
+import jakarta.persistence.*
 import java.util.*
 
-@Entity
-@NoArgsConstructor
-@Data
+@Entity(name = "messages")
 data class Message(
 
+    @Column(name = "date_sent", nullable = false)
     val dateSent: Date = Date(),
 
+    @Column(name = "type", nullable = false)
     val type: MessageType = MessageType.SMS,
 
+    @Column(name = "sender", nullable = false)
     val sender: String = "",
 
+    @Column(name = "receiver", nullable = false)
     val receiver: String = "",
 
+    @Column(name = "content", nullable = false)
     val content: String = "",
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     val id: Long? = null,
 
+    @Column(name = "created_at", nullable = false)
     val createdAt: Date = Date(),
 
+    @Column(name = "updated_at", nullable = false)
     val updatedAt: Date = Date(),
 
+    @Column(name = "deleted_at", nullable = true)
     val deletedAt: Date? = null
 )
